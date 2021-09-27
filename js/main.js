b0a60fd7d07467d34205f213ff0ad1e209eb5aef
Coconyan <San3k19@gmail.com>
2021-09-27 18:16:49 +0700

    improve functions with ternary operator

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
 function getRandomInt (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if (min >= max) {
-    return 'Максимальное значение равно, либо меньше минимального.';
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return (min >= max) ? 'Максимальное значение равно, либо меньше минимального.' : Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 getRandomInt(0, 20);
 
 function getRandomFloatInt (min, max, decimal) {
-  if (min >= max) {
-    return 'Максимальное значение равно, либо меньше минимального.';
-  }
-  return +(Math.random() * (max - min + 1) + min).toFixed(decimal);
+  return (min >= max) ? 'Максимальное значение равно, либо меньше минимального.' : +(Math.random() * (max - min + 1) + min).toFixed(decimal);
 }
 
-getRandomFloatInt(4.52, 4.65, 2);
+getRandomFloatInt(0.52, 4, 3);
